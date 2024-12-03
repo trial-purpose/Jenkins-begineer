422e7827f37f01f50fa521654845c970e0e58db2
trial-purpose <9182736455ga@gmail.com>
2024-12-03 23:55:39 +0530

    first commit

---
+++ AEIOU.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class AEIOU {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your String");
+        String s = sc.next();
+        int count =0;
+        String s1 = "AEIOUaeiou";
+
+        for (int i=0; i < s.length(); i++){
+            if(s1.contains(String.valueOf(s.charAt(i)))){
+                count++;
+                System.out.print(s.charAt(i));
+            }
+        }
+        System.out.println("\nNumber of vowels: " + count);
+
+    }
+}
